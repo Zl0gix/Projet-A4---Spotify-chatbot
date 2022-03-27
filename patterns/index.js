41a55ebeb83543279b4b1b8708b583c839a56c5a
Thibaut ROUET <thibaut.rouet@yahoo.fr>
2022-03-27 16:53:38 +0200

    Dossiers créés

---
+++ patterns/index.js
@@ -0,0 +1,28 @@
+const patternArr = [
+	{
+		pattern:"What(?:'s|\\sis|\\sare)\\sthe\\smusic\\s(?:styles?|genres?)\\sof\\s(?<Artist>.+)\\s\\?$",
+		intent: "Style of artist"
+	},
+	{
+		pattern:"Who\\sdid\\s(?<Track>.+)\\s\\?$",
+		intent: "Artist from track"
+	},
+	{
+		pattern:"What(?:'s| is)\\sthe\\salbum\\sof\\s(?<Track>.+)\\s\\?$",
+		intent: "Album from track"
+	},
+	{
+		pattern:"What(?:'s|\\sis|\\sare)\\sthe\\smost\\s(?:listened|popular)\\salbums?\\sof\\s(?<Track>.+)\\s\\?$",
+		intent: "Most listened album"
+	},
+	{
+		pattern:"Is\\s(?<Artist1>.+)\\smore\\spopular\\sthan\\s(?<Artist2>.+)\\s\\?$",
+		intent: "Artists popularity"
+	},
+	{
+		pattern:"Is\\s(?<Track1>.+)\\smore\\slistened\\sthan\\s(?<Track2>.+)\\s\\?$",
+		intent: "Tracks popularity"
+	}
+];
+
+module.exports = patternArr;
